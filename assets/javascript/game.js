48712b82e1c9aab7017b2b87897986b888a59f1f
Laura Boetcher <g33klaura@gmail.com>
2017-08-06 23:26:44 -0400

    Add areas for score keeping to html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
 /* Steps needed for the game part
-* Need variables for computerChoice, playerGuess, Wins, Losses, guessedLetters, computerReveal
+* Need variables for computerChoice, playerGuess, Wins, Losses, chancesLeft, guessedLetters, computerReveal
 * Computer chooses a letter
+*	- Keeps same letter for a number of playerGuess (a round)
 * User guesses a letter by pressing it on the keyboard
 * If valid letter:
 * 	a) Guessed letters are recorded on page
@@ -17,4 +18,33 @@
 * WANT COMPUTER CHOICE TO SHOW ON PAGE NO MATTER WHAT before new round begins (computerReveal)
 */
 
-let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+// Valid keyboard choices
+let choices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+// Scoring storage
+let wins = 0;
+let losses = 0;
+
+
+// Still will need computerChoice, playerGuess, guessedLetters, chancesLeft, computerReveal
+
+
+// Stores letter from user pressing key on keyboard
+
+    document.onkeyup = function() {
+        var playerGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+        console.log(playerGuess);
+    
+    // Has computer choose from array choices
+    // Needs to NOT pick new letter each time!!!!!!
+
+    	var computerChoice = choices[Math.floor(Math.random()*choices.length)];
+
+    	console.log(computerChoice);
+
+// Trying piece from jsfiddle, wtf does '$' mean in js??
+		$('body').append('<p>The computer chose: ' + computerChoice + '</p>');
+
+    }
