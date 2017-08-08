b37628443a776e4e49e4380f213b1a38b3d88a5d
Laura Boetcher <g33klaura@gmail.com>
2017-08-07 20:16:07 -0400

    Tryin' stuff, seein' what sticks

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,57 +1,90 @@
 /* Steps needed for the game part
-* Need variables for computerChoice, playerGuess, Wins, Losses, chancesLeft, guessedLetters, computerReveal
-* Computer chooses a letter
-*	- Keeps same letter for a number of playerGuess (a round)
-* User guesses a letter by pressing it on the keyboard
-* If valid letter:
-* 	a) Guessed letters are recorded on page
-* 	b) Guesses count against "number of guesses"
-* If invalid letter:
-* 	a) Prompt "not a letter!"
-* 	b) Doesn't count against number of guesses
-* If same letter already guessed, don't count against number of guesses
-* Their choice either a) matches computer choice, or b) doesn't match
-* 	a) If matches, Win gets logged, 
-*			prompt win, new round begins
-* 	b) If no match, Loss gets logged, 
-*			prompt lose, new round begins
-* WANT COMPUTER CHOICE TO SHOW ON PAGE NO MATTER WHAT before new round begins (computerReveal)
-* Display game over before new round starts
-*/
+ * Need variables for computerChoice, playerGuess, Wins, Losses, chancesLeft, guessedLetters, computerReveal
+ * Computer chooses a letter
+ *	- Keeps same letter for a number of playerGuess (a round)
+ * User guesses a letter by pressing it on the keyboard
+ * If valid letter:
+ * 	a) Guessed letters are recorded on page
+ * 	b) Guesses count against "number of guesses"
+ * If invalid letter:
+ * 	a) Prompt "not a letter!"
+ * 	b) Doesn't count against number of guesses
+ * If same letter already guessed, don't count against number of guesses
+ * Their choice either a) matches computer choice, or b) doesn't match
+ * 	a) If matches, Win gets logged, 
+ *			prompt win, new round begins
+ * 	b) If no match, Loss gets logged, 
+ *			prompt lose, new round begins
+ * WANT COMPUTER CHOICE TO SHOW ON PAGE NO MATTER WHAT before new round begins (computerReveal)
+ * Display game over before new round starts
+ */
 
 
-// Valid keyboard choices
+// Valid choices in array
 var choices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 // Variables for everything other than choices array
 var wins = 0;
 var losses = 0;
-var chancesLeft = 8;  //Use 8 for now, can change later
+var chancesLeft = 8; //Use 8 for now, can change later
+
+var computerReveal = [];
+
 var guessedLetters = [];
-var playerGuess = [];
-var computerChoice = [];
 
 
-// Still will need computerChoice, playerGuess, guessedLetters, computerReveal
-
+// Does this ENTIRE thing go in ONE function??
 
 // Stores letter from user pressing key on keyboard
+document.onkeyup = function(event) {
+        var playerGuess = String.fromCharCode(event.keyCode).toLowerCase();
+        	console.log(playerGuess);
 
-    document.onkeyup = function(event) {
-    	choices = event.key;
-        playerGuess = String.fromCharCode(event.keyCode).toLowerCase();
+        	guessedLetters.push(playerGuess)
+        	document.getElementById('#game').innerHTML = guessedLetters.toString('<p>Letters you\'ve guessed: </p>');
 
-        console.log(playerGuess);
-    
-    // Has computer choose from array choices
-    // Needs to NOT pick new letter each time!!!!!!
 
-    	computerChoice = choices[Math.floor(Math.random()*choices.length)];
+        	// Put here? the limits on what's a letter vs not?
+        	/*
+        	if (playerGuess === [choices.indexOf]) {  //This isn't working...
+        		console.log(playerGuess);
+        	} else {
+        		alert('Hey dummy, choose a letter!');
+        	}
+        	*/
 
-    	console.log(computerChoice);
+        // Has computer choose from array options
+        // Needs to NOT pick new letter each time!!!!!!
+
+        var computerGuess = choices[Math.floor(Math.random() * choices.length)];
+
+        console.log(computerGuess);
+
+// Needs a FOR loop, to determine how many times to allow guesses
+
+	//	for ()
+
+
+     	// Variable to store the scoring in html on page
+/*
+     	var html = '<p>Press r (for rock), p (for paper), or s (for scissors) to start playing!</p>' +
+     	"<p>Wins: " + wins + "</p>" +
+     	"<p>Losses: " + losses + "</p>" +
+     	"<p>Ties: " + ties + "</p>";
+
+     	document.querySelector('#game').innerHTML = html;
+
     }
+*/
 
-// Trying piece from jsfiddle, wtf does '$' mean in js??
-	/*	$('body').append('<p>The computer chose: ' + computerChoice + '</p>');
+}  // Closes the whole function, starting at document.onkeyup
+
+
+
+
+
+
+        // Trying piece from jsfiddle, wtf does '$' mean in js??
+        /*	$('body').append('<p>The computer chose: ' + computerChoice + '</p>');
 
     } */
